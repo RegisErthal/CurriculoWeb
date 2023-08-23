c2f72fa15202f5981ebb70f9a58c5404b2a9cff1
Régis Erthal <45245812+RegisErthal@users.noreply.github.com>
2023-08-22 23:29:57 -0300

    Add carrossel de formacoes

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ Controllers/FormacoesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class FormacoesController : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
